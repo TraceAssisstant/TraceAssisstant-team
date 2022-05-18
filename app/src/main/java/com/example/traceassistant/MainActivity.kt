e38df5afc2738c4dc2f1e59aad3e58acc382c519
ikbye <934324304@qq.com>
2022-05-18 11:35:42 +0800

    增加了图文显示功能的api

--- app/src/main/java/com/example/traceassistant/MainActivity.kt
+++ app/src/main/java/com/example/traceassistant/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.traceassistant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
+import android.util.Log
 import com.example.traceassistant.databinding.ActivityMainBinding
+import com.example.traceassistant.logic.Database.AppDatabase
+import com.example.traceassistant.logic.Entity.SignNature
+import kotlin.concurrent.thread
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -11,5 +15,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val sndao = AppDatabase.getDatabase(this).SignNatureDao()
+
+        val signNature1= SignNature("you life","image/image01.jpg")   //测试用例
+        val signNature2= SignNature("my life","image/image02.jpg")
+
+
+        thread {
+            sndao.insertSN(signNature1)
+            sndao.insertSN(signNature2)    //添加数据测试
+        }
+
+        thread{
+
+
+           Log.d("根据id查找图文信息:",sndao.loadSNById(1).toString())
+
+        }
+        thread {
+            for (sn in sndao.SNList()){
+                Log.d("对sn图文的遍历:",sn.toString())
+            }
+        }
+
+        thread{
+            sndao.deleteSNById(2);
+
+        }
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/traceassistant/logic/Entity/SignNature.kt
@@ -0,0 +1,16 @@
+package com.example.traceassistant.logic.Entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+//图片文字数据类(id是当前日期(星期一 id就是1 ),从1 开始)
+@Entity
+data class SignNature(val text:String, val imageUrl:String){
+        @PrimaryKey(autoGenerate = true)    //自动生成主键id
+        var id:Int = 0;
+        override fun toString(): String {
+                return "id(day): "+id+" "+super.toString()
+        }
+}
+
+
