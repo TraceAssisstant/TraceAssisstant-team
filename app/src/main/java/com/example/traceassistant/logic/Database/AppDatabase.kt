00889fbc5c81a199070fac01edf647cf24cb40eb
ikbye <934324304@qq.com>
2022-05-25 17:03:07 +0800

    数据库正式版本1.0发布
    1. 规范化了数据库表和实例属性名的命名规范,创建了AppDatabase1.0
    2. 并修改了SN表中的imageUrl为unique
    3. 注意事项:编写kt文件调用数据库的插入操作时要进行trycatch错误捕获,防止出现闪退的现象.

--- app/src/main/java/com/example/traceassistant/logic/Database/AppDatabase.kt
+++ app/src/main/java/com/example/traceassistant/logic/Database/AppDatabase.kt
@@ -6,8 +6,8 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.traceassistant.logic.Dao.SignNatureDao
 import com.example.traceassistant.logic.Entity.SignNature
-
-@Database(version = 2, entities = [SignNature::class])
+//数据库正式版本发布
+@Database(version = 1, entities = [SignNature::class])
 abstract class AppDatabase : RoomDatabase() {
     abstract fun SignNatureDao(): SignNatureDao
 
