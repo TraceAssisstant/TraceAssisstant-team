67258126531ba080b4f2dff2ec0c5e103f83b107
ikbye <934324304@qq.com>
2022-05-25 16:54:54 +0800

    Default Changelist

--- app/src/main/java/com/example/traceassistant/logic/Database/AppDatabase.kt
+++ app/src/main/java/com/example/traceassistant/logic/Database/AppDatabase.kt
@@ -7,18 +7,24 @@ import androidx.room.RoomDatabase
 import com.example.traceassistant.logic.Dao.SignNatureDao
 import com.example.traceassistant.logic.Entity.SignNature
 
-@Database(version = 1, entities = [SignNature::class])
+@Database(version = 2, entities = [SignNature::class])
 abstract class AppDatabase : RoomDatabase() {
     abstract fun SignNatureDao(): SignNatureDao
+
+
     companion object {
+
         private var instance: AppDatabase? = null
+
         @Synchronized
         fun getDatabase(context: Context): AppDatabase {
             instance?.let {
                 return it
             }
+
             return Room.databaseBuilder(context.applicationContext,
-                AppDatabase::class.java, "sign_nature")
+                AppDatabase::class.java, "AppDatabase")
+                .fallbackToDestructiveMigration()
                 .build().apply {
                     instance = this
                 }
--- app/src/main/java/com/example/traceassistant/logic/Entity/SignNature.kt
+++ app/src/main/java/com/example/traceassistant/logic/Entity/SignNature.kt
@@ -1,13 +1,21 @@
 package com.example.traceassistant.logic.Entity
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
 //图片文字数据类(id是当前日期(星期一 id就是1 ),从1 开始)
-@Entity
-data class SignNature(val text:String, val imageUrl:Int){
-        @PrimaryKey(autoGenerate = true)    //自动生成主键id
-        var id:Int = 0;
+@Entity(tableName = "sign_nature",  indices = [Index(value = ["image_url"],unique = true)])
+data class SignNature(
+        @ColumnInfo(name = "text") val text:String,
+        @ColumnInfo(name = "image_url") val imageUrl:Int
+
+){
+        @PrimaryKey(autoGenerate = true)
+        var id:Int = 0
+
+
         override fun toString(): String {
                 return "id(day): "+id+" text :"+text+" imgurl: "+imageUrl
         }
