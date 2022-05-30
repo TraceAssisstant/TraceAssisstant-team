c718aa98ee51e760e8f60687ff9f255052500939
cx330 <3200611062@stmail.ujs.edu.cn>
2022-05-30 23:55:56 +0800

    切换页面改变图片，修改lastid

--- app/src/main/java/com/example/traceassistant/ui/main/MainView.kt
+++ app/src/main/java/com/example/traceassistant/ui/main/MainView.kt
@@ -1,72 +1,83 @@
 package com.example.traceassistant.ui.main
 
-import android.content.BroadcastReceiver
-import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.*
 import com.example.traceassistant.R
 import com.example.traceassistant.databinding.ActivityMainViewBinding
 import com.example.traceassistant.logic.Entity.SignNature
-import com.example.traceassistant.logic.Repository
 import com.example.traceassistant.ui.affairsCollection.CollectionView
-import com.example.traceassistant.ui.affairsCollection.addElementsView
+
 
 class MainView : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainViewBinding
     val viewModel by lazy { ViewModelProvider(this).get(MainViewModel::class.java) }
 
+    override fun onResume() {
+        var lastId = viewModel.loadRepository()
+        super.onResume()
+        var id = 0
+//            实现任意切换
+//            val id = (1..4).random()
+
+//            实现递增切换
+        if (id < lastId) {
+            id++
+            viewModel.showSN(id)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainViewBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
 
 //        @cx330测试
-      viewModel.loadRepository()
+        var lastId = viewModel.loadRepository()
+
+        fun getImageRes(imgIndex: Int): Int {
+            return resources.getIdentifier("background$imgIndex", "drawable", packageName)
+        }
+        Log.d("img", getImageRes(1).toString())
 
 
-        //        测试代码@cx330
+//        测试代码@ cx330
 //        点击图片切换背景，签名
-        var id=0
+
+        var id = 0
         binding.mainImage.setOnClickListener {
 //            实现任意切换
 //            val id = (1..4).random()
 
 //            实现递增切换
-            if (id<5){
+            if (id < lastId) {
                 id++
                 viewModel.showSN(id)
 
             }
         }
 
-
-        viewModel.mainViewLiveData.observe(this, Observer { result->
+        viewModel.mainViewLiveData.observe(this, Observer { result ->
             val signature = result.getOrNull()
 //            Toast.makeText(this, "kkk", Toast.LENGTH_SHORT).show()
 
-            if (signature!=null){
-                val (str,id) = signature as SignNature
+            if (signature != null) {
+                val (str, id) = signature as SignNature
 //                Toast.makeText(this, "qqq", Toast.LENGTH_SHORT).show()
 
                 binding.daySentence.text = str
                 binding.mainImage.setImageResource(id)
 
-            }else{
-                //                Toast.makeText(this, "tt", Toast.LENGTH_SHORT).show()
+            } else {
+//                Toast.makeText(this, "tt", Toast.LENGTH_SHORT).show()
 
             }
-
         }
         )
-
-
 
 
 //        以下代码皆仅是测试
