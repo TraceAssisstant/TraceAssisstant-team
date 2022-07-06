fc04dd8fba75b9ff8fd8883d7a288abaef891cbe
ikbye <934324304@qq.com>
2022-07-06 18:49:36 +0800

    数据库2.0，增加了事务表，同步到主分支

--- app/src/main/java/com/example/traceassistant/logic/Database/AppDatabase.kt
+++ app/src/main/java/com/example/traceassistant/logic/Database/AppDatabase.kt
@@ -4,12 +4,15 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.traceassistant.logic.Dao.AffairFormDao
 import com.example.traceassistant.logic.Dao.SignNatureDao
+import com.example.traceassistant.logic.Entity.AffairForm
 import com.example.traceassistant.logic.Entity.SignNature
 
-@Database(version = 1, entities = [SignNature::class])
+@Database(version = 2, entities = [SignNature::class, AffairForm::class])
 abstract class AppDatabase : RoomDatabase() {
     abstract fun SignNatureDao(): SignNatureDao
+    abstract fun affairFormDao(): AffairFormDao
     companion object {
         private var instance: AppDatabase? = null
         @Synchronized
