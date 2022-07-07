f991b1b7ef5364a06e15d12e6460e76e307ea9a0
cx330 <3200611062@stmail.ujs.edu.cn>
2022-07-07 23:12:49 +0800

    modify：将点击切换背景换为每隔一段时间改变背景

--- app/src/main/java/com/example/traceassistant/ui/main/MainView.kt
+++ app/src/main/java/com/example/traceassistant/ui/main/MainView.kt
@@ -1,33 +1,34 @@
 package com.example.traceassistant.ui.main
 
 import android.content.Intent
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.*
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.traceassistant.R
 import com.example.traceassistant.databinding.ActivityMainViewBinding
 import com.example.traceassistant.logic.Entity.SignNature
 import com.example.traceassistant.ui.affairsCollection.CollectionView
+import java.util.*
+import kotlin.concurrent.timer
 
 
 class MainView : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainViewBinding
     val viewModel by lazy { ViewModelProvider(this).get(MainViewModel::class.java) }
-
-    override fun onResume() {
-        var lastId = viewModel.loadRepository()
-        super.onResume()
-        var id = 0
-//            实现任意切换
-//            val id = (1..4).random()
-
-//            实现递增切换
-        if (id < lastId) {
-            id++
-            viewModel.showSN(id)
+    val timer = Timer(true)
+    var id = 0
+    var lastId=4
+    val date = Date(System.currentTimeMillis())
+    val timerTask: TimerTask = object : TimerTask() {
+        override fun run() {
+            if (id<lastId){
+                id++
+                viewModel.getSN(id)
+                Log.d("idtime",id.toString())
+            }
         }
     }
 
@@ -35,57 +36,39 @@ class MainView : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainViewBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-//        @cx330测试
-        var lastId = viewModel.loadRepository()
-
-        fun getImageRes(imgIndex: Int): Int {
-            return resources.getIdentifier("background$imgIndex", "drawable", packageName)
-        }
-        Log.d("img", getImageRes(1).toString())
-
-
-//        测试代码@ cx330
-//        点击图片切换背景，签名
-
-        var id = 0
-        binding.mainImage.setOnClickListener {
-//            实现任意切换
-//            val id = (1..4).random()
-
-//            实现递增切换
-            if (id < lastId) {
-                id++
-                viewModel.showSN(id)
-
-            }
-        }
+        viewModel.loadRepository()
 
         viewModel.mainViewLiveData.observe(this, Observer { result ->
             val signature = result.getOrNull()
-//            Toast.makeText(this, "kkk", Toast.LENGTH_SHORT).show()
-
             if (signature != null) {
                 val (str, id) = signature as SignNature
-//                Toast.makeText(this, "qqq", Toast.LENGTH_SHORT).show()
-
                 binding.daySentence.text = str
                 binding.mainImage.setImageResource(id)
-
-            } else {
-//                Toast.makeText(this, "tt", Toast.LENGTH_SHORT).show()
 
             }
         }
         )
 
 
-//        以下代码皆仅是测试
-//        向“添加事务”按钮添加跳转到事务收集页面的方法
+        //        以下代码皆仅是测试
+        //        向“添加事务”按钮添加跳转到事务收集页面的方法
         binding.addButton.setOnClickListener() {
             val intent = Intent(this, CollectionView::class.java)
             startActivity(intent)
         }
     }
 
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        timer.schedule(timerTask, date, 1000)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        timer.cancel()
+    }
 }
--- app/src/main/java/com/example/traceassistant/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/traceassistant/ui/main/MainViewModel.kt
@@ -9,37 +9,34 @@ import com.example.traceassistant.R
 import com.example.traceassistant.logic.Repository
 
 class MainViewModel : ViewModel() {
-    private val snLiveData = MutableLiveData<String>()
+    private val snLiveData = MutableLiveData<Int>()
 
     val mainViewLiveData =
         Transformations.switchMap(snLiveData) { id -> Repository.loadSNById(id.toInt()) }
 
-    fun showSN(id: Int) {
-        snLiveData.value = id.toString()
+    fun getSN(id: Int) {
+        snLiveData.postValue(id)
     }
 
-
-//    加载数据库资源
+    //    加载数据库资源
     fun loadRepository(): Int {
         Repository.initSndao()
-//     批量插入图片签名资源
+    //     批量插入图片签名资源
         var strList = mutableListOf<String>()
         var imageList = mutableListOf<Int>()
         var lastId = 0
-
         for (k in R.drawable.background01..R.drawable.background05) {
             imageList.add(k)
             strList.add("签名${k}")
-            lastId ++
+            lastId++
             Log.d("k", k.toString())
         }
         Log.d("lastid", lastId.toString())
 
 
-//    避免数据重复插入
+    //    避免数据重复插入
         if (Repository.loadSNById(lastId) == null) {
             Repository.batchInsertSN(strList, imageList)
-
         }
 
         Repository.SNList()
